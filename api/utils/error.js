907e616173cd63a893facb05665300fad39dfa57
Subham <mr.subhamdas999@gmail.com>
2025-02-08 11:30:23 +0530

    complete sign up page ui

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,4 +1,4 @@
-export const errorHandler = (statusCode,message)=>{
+export const errorHandler = (statusCode,message)=>{  //this error is created by us this is no internal server error this is basically when usen `s data is not correct and data is short(like password)
     const error=new Error();
     error.statusCode=statusCode;
     error.message=message;
